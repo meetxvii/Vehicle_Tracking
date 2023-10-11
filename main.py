2a359bba9883282393e5aeb7e476c62706adc344
Meet Makwana <meet.m@scaledge.io>
2023-10-11 11:39:16 +0530

    Vehicle Counting

--- main.py
+++ main.py
@@ -224,3 +224,131 @@ elif app_mode == "Track From Video":
             fps_markdown.markdown(f"## {fps:.2f}")
         
         vid.release()
+
+        
+            
+elif app_mode == "Count Vehicles From Video":
+        
+    bbox_color = st.sidebar.color_picker("Bbox Color", "#ffffff")
+    st.sidebar.markdown("---")
+    
+    detection_threshold = st.sidebar.slider("Detection Threshold", 0.0, 1.0, 0.5, 0.01)
+    
+    st.sidebar.markdown("---")
+    
+    if torch.cuda.is_available():
+        enable_gpu = st.sidebar.checkbox("Enable GPU", False)
+        st.sidebar.markdown("---")
+
+    video_file_buffer = st.sidebar.file_uploader("Upload a video", type=["mp4"])
+    tffile = tempfile.NamedTemporaryFile(delete=False)
+
+    in_ids = set()
+    out_ids = set()
+    in_line = ((500,700),(920,700))
+    out_line = ((950,700),(1300,700))
+
+
+
+    if video_file_buffer is not None:
+        bbox_color = hex2rgb(bbox_color)[::-1]
+        col1,col2,col3 = st.columns(3)
+        with col1:
+            st.markdown("## In Count: ")
+            in_markdown = st.markdown(f"## {len(in_ids)}")
+        with col2:
+            st.markdown("## Out Count: ")
+            out_markdown = st.markdown(f"## {len(out_ids)}")
+        with col3:
+            st.markdown("## FPS: ")
+            fps_markdown = st.markdown(f"## 0")
+        stframe = st.empty()
+
+        tffile.write(video_file_buffer.read())
+
+        vid = cv2.VideoCapture(tffile.name)
+
+
+        st.sidebar.text("Input Video")
+        st.sidebar.video(tffile.name)
+
+        fps = 0
+        frame_count = 0
+        start_time = time.time()
+
+        while vid.isOpened():
+            
+            ret,frame = vid.read()
+
+            if not ret:
+                break
+            in_line_color = (0,255,0)
+            out_line_color = (0,0,255)
+
+            if enable_gpu:
+                results = model(frame,device=0)
+            else:
+                results = model(frame,device='cpu')
+
+            for result in results:
+                detections = []
+                for r in result.boxes.data.tolist():
+                    x1, y1, x2, y2, score, class_id = r
+                    x1 = int(x1)
+                    x2 = int(x2)
+                    y1 = int(y1)
+                    y2 = int(y2)
+                    class_id = int(class_id)
+                    if score > detection_threshold:
+                        detections.append([x1, y1, x2, y2, score])
+            if len(detections) > 0:
+                tracker.update(frame,detections)
+            
+            if tracker.tracks is not None:
+                for track in tracker.tracks:
+                    
+                    bbox = track.bbox
+                    x1, y1, x2, y2 = map(int,bbox)
+                    center = (int((x1+x2)/2),int((y1+y2)/2))
+                    track_id = track.track_id
+
+                    cv2.line(frame,(x1,y1),(x1+line_length,y1),bbox_color,2)
+                    cv2.line(frame, (x1, y1), (x1, y1+ line_length), bbox_color, 2)
+
+                    cv2.line(frame, (x2, y1), (x2 - line_length, y1), bbox_color, 2)
+                    cv2.line(frame, (x2, y1), (x2, y1 + line_length), bbox_color, 2)
+
+                    cv2.line(frame, (x1, y2), (x1 + line_length, y2), bbox_color, 2)
+                    cv2.line(frame, (x1, y2), (x1 , y2 - line_length), bbox_color, 2)
+
+                    cv2.line(frame, (x2, y2), (x2-line_length, y2), bbox_color, 2)
+                    cv2.line(frame, (x2, y2), (x2, y2-line_length), bbox_color, 2)
+                    cv2.putText(frame, f"ID: {track_id}", (x1, y1 - 10), 0, .5, bbox_color, 2)
+                    
+                    if center[0] >= in_line[0][0] and center[1] >= in_line[0][1]-5 and center[0] <= in_line[1][0] and center[1] <= in_line[1][1]+5:
+                        in_line_color = (255,255,255)
+                        in_ids.add(track_id)
+                        in_markdown.markdown(f"## {len(in_ids)}")
+                    
+                    if center[0] >= out_line[0][0] and center[1] >= out_line[0][1]-5 and center[0] <= out_line[1][0] and center[1] <= out_line[1][1]+5:
+                        out_line_color = (255,255,255)
+                        out_ids.add(track_id)
+                        out_markdown.markdown(f"## {len(out_ids)}")
+                    
+                    
+    
+            cv2.line(frame,in_line[0],in_line[1],in_line_color,2)
+            
+            cv2.line(frame, out_line[0], out_line[1], out_line_color, 2)
+
+            frame.flags.writeable = True
+            stframe.image(frame, channels="BGR", use_column_width=True)
+            
+            frame_count+=1
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 1:
+                fps = frame_count/elapsed_time
+                frame_count = 0
+                start_time = time.time()
+            
+            fps_markdown.markdown(f"## {fps:.2f}")
